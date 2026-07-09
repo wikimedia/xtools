wikimedia/xtools

commit 9fc0b83f381964340f830a8e51c52565723a5b05

RateLimitSubscriber: require login for suspicious paths
This puts into version control what has been running in prod for some
time now. This is not a solution but a bandage.

Bug: T384711

src/EventSubscriber/RateLimitSubscriber.php
@@ -36,16 +36,6 @@ class RateLimitSubscriber implements EventSubscriberInterface
         'showAction',
     ];
 
-    /**
-     * Maximum number of requests to the same URI with different interface languages.
-     */
-    public const MAX_CRAWLER_COUNT = 3;
-
-    /**
-     * Duration in which the max number of requests to the same URI with different interface languages is allowed.
-     */
-    public const MAX_CRAWLER_TIME = 'PT1M';
-
     protected CacheItemPoolInterface $cache;
     protected I18nHelper $i18n;
     protected LoggerInterface $crawlerLogger;
@@ -202,33 +192,14 @@ class RateLimitSubscriber implements EventSubscriberInterface
 
         $useLang = $useLangMatches[1];
 
-        // Requesting a language that's different than that of the target project.
-        if (1 === preg_match("/[=\/]$useLang.wik/", $this->uri)) {
+        // If requesting the same language as the target project, ignore.
+        // FIXME: This has side-effects (T384711#10759078)
+        if (1 === preg_match("/[=\/]$useLang.?wik/", $this->uri)) {
             return;
         }
 
-        // We're trying to check if everything BUT the uselang has remained unchanged.
-        $cacheUri = str_replace('uselang='.$useLang, '', $this->uri);
-        $cacheKey = 'ratelimit.crawler.'.sha1($this->userAgent.$cacheUri);
-        $cacheItem = $this->cache->getItem($cacheKey);
-
-        // If increment value already in cache, or start with 1.
-        $count = $cacheItem->isHit() ? (int)$cacheItem->get() + 1 : 1;
-
-        // Check if limit has been exceeded, and if so, add a log entry.
-        if ($count > 3) {
-            $this->crawlerLogger->info('Possible crawler detected');
-        }
-
-        // Reset the clock on every request.
-        $cacheItem->set($count)
-            ->expiresAfter(new DateInterval(self::MAX_CRAWLER_TIME));
-        $this->cache->save($cacheItem);
-
-        // If we've got a lot of hits, let's go ahead and assume it's a crawler and give a 429.
-        if ($count > self::MAX_CRAWLER_COUNT) {
-            $this->denyAccess('Web crawler detected');
-        }
+        // Require login.
+        throw new AccessDeniedHttpException('error-login-required');
     }
 
     /**

commit 3e7887835f6545cab4687d828547169517ce4283

RateLimitSubscriber: allow subrequests with uselang query string param
This has been broken since commit 9fc0b83f38 (June 2025). When loading a
result page that makes an API request to another XTools tool – aka
subrequets – it was getting blocked because we weren't parsing the
uselang parameter correctly. Try for example:

  /autoedits/en.wikipedia.org/MusikPuppet?uselang=en

This patch allows the "Non-automated edits" subrequest to get a
successful response.

Change-Id: I0ba85d1a3031c90a54d6f2b91f76e5d4d3086aba

src/EventSubscriber/RateLimitSubscriber.php
@@ -161,20 +161,20 @@ class RateLimitSubscriber implements EventSubscriberInterface {
 	 * but with a different interface language, as happens when it is crawling the language dropdown in the UI.
 	 */
 	private function logCrawlers(): void {
-		$useLangMatches = [];
-		$hasMatch = preg_match( '/\?uselang=(.*)/', $this->uri, $useLangMatches );
-
-		if ( $hasMatch !== 1 ) {
+		if ( !$this->request->query->has( 'uselang' ) ) {
 			return;
 		}
 
-		$useLang = $useLangMatches[1];
+		$useLang = $this->request->query->get( 'uselang' );
 
 		// If requesting the same language as the target project, ignore.
 		// FIXME: This has side-effects (T384711#10759078)
 		if ( preg_match( '/[=\/]' . preg_quote( $useLang ) . '.?wik/', $this->uri ) === 1 ) {
 			return;
 		}
+
+		$clientIp = $this->request->getClientIp() ?? '(unknown)';
+		$this->crawlerLogger->info( "Possible crawler detected for $clientIp" );
 
 		// Require login.
 		throw new AccessDeniedHttpException( 'error-login-required' );